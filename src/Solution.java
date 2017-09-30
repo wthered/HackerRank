34ea6ca4bd15f0cfa10662a01b2ba5009c94a9bc
wthered <wthered@gmail.com>
2017-09-30 20:33:01 +0300

    The Snake vs the Wind

--- src/Solution.java
+++ src/Solution.java
@@ -13,38 +13,42 @@ public class Solution {
 
 		switch (d) {
 			case 'n':
-				moveNorth(x, y, n, 0, terrain, d);
+				moveNorth(x, y, n, 0, terrain);
 				break;
 			case 's':
-				moveSouth(x, y, n, 0, terrain, d);
+				moveSouth(x, y, n, 0, terrain);
 				break;
 			case 'e':
-				moveEast(x, y, n, 0, terrain, d);
+				moveEast(x, y, n, 0, terrain);
 				break;
 			case 'w':
-				moveWest(x, y, n, 0, terrain, d);
+				moveWest(x, y, n, 0, terrain);
 				break;
 			default:
-				moveNorth(0,0, n, 0, terrain, 'd');
+				moveNorth(0,0, n, 0, terrain);
 				break;
 		}
 	}
 
-	private static void moveWest(int x, int y, int n, int time, int[][] terrain, char wind) {
-		for(int j=n-1; j>=0; j--) {
+	private static void moveWest(int x, int y, int n, int time, int[][] terrain) {
+		for(int j=y; j>=0; j--) {
 			time++;
 			terrain[x][j] = time;
 		}
 
 		if(time < n * n) {
 			// The Snake is at (x,y) and moves East
-			moveEast(x+1, y, n, time, terrain, wind);
+			if(x == 0) {
+				moveEast(x + 1, y, n, time, terrain);
+			} else {
+				moveEast(x - 1, y, n, time, terrain);
+			}
 		} else {
 			show(terrain, n);
 		}
 	}
 
-	private static void moveEast(int x, int y, int n, int time, int[][] terrain, char wind) {
+	private static void moveEast(int x, int y, int n, int time, int[][] terrain) {
 		for(int j=y; j<n; j++) {
 			time++;
 			terrain[x][j] = time;
@@ -52,27 +56,35 @@ public class Solution {
 
 		if(time < n * n) {
 			// The snake is at (x,y) and moves West
-			moveWest(x+1, y, n, time, terrain, wind);
+			if(x == n-1) {
+				moveWest(x - 1, y, n, time, terrain);
+			} else {
+				moveWest(x + 1, y, n, time, terrain);
+			}
 		} else {
 			show(terrain, n);
 		}
 	}
 
-	private static void moveNorth(int x, int y, int n, int time, int[][] terrain, char wind) {
-		for(int i=n-1; i>=0; i--) {
+	private static void moveNorth(int x, int y, int n, int time, int[][] terrain) {
+		for(int i=x; i>=0; i--) {
 			time++;
 			terrain[i][y] = time;
 		}
 
 		if(time < n * n) {
 			//Start from (x,y) and go south
-			moveSouth(x,y+1, n, time, terrain, wind);
+			if(y == 0) {
+				moveSouth(x, y + 1, n, time, terrain);
+			} else {
+				moveSouth(x, y - 1, n, time, terrain);
+			}
 		} else {
 			show(terrain, n);
 		}
 	}
 
-	private static void moveSouth(int x, int y, int n, int time, int[][] terrain, char wind) {
+	private static void moveSouth(int x, int y, int n, int time, int[][] terrain) {
 		for(int i=0; i<n; i++) {
 			time++;
 			terrain[i][y] = time;
@@ -80,7 +92,11 @@ public class Solution {
 
 		if(time < n * n) {
 			//Start from (x,y) and go north
-			moveNorth(x,y+1, n, time, terrain, wind);
+			if(y == 0) {
+				moveNorth(n - 1, y + 1, n, time, terrain);
+			} else {
+				moveNorth(n - 1, y - 1, n, time, terrain);
+			}
 		} else {
 			show(terrain, n);
 		}
