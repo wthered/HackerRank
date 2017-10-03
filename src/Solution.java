dcc3127e3e01c97ea806e00dfb1ad07c32c3758f
wthered <wthered@gmail.com>
2017-10-03 17:44:52 +0300

    Left Rotation
    Signed-off-by: wthered <wthered@gmail.com>

--- src/Solution.java
+++ src/Solution.java
@@ -1,63 +1,19 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Solution {
-	public static void main(String args[] ) throws Exception {
-        Scanner input = new Scanner(System.in);
-        int t = input.nextInt();
-        ArrayList<Integer> chocolates = new ArrayList<>();
-        for(int i=0; i<t; i++) {
-        	int n = input.nextInt();
-        	int min = 1024;
-        	int max = 0;
-        	int minIndex = n;
-        	int maxIndex = 0;
-        	int[] chocos = new int[n];
-        	for(int c=0; c<n; c++) {
-        		chocos[c] = input.nextInt();
-        		if(chocos[c] < min) { min = chocos[c]; minIndex = c; }
-        		if(chocos[c] > max) { max = chocos[c]; maxIndex = c; }
-	        }
-	        chocolates.add(share(chocos, minIndex, maxIndex));
-        }
-        input.close();
 
-		for (Integer chocolate : chocolates) {
-			System.out.println("She gave at " + chocolate + " rounds");
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int d = in.nextInt();
+		int[] list = new int[n];
+		for(int i = 0; i < n; i++){
+			list[(i+n-d) % n] = in.nextInt();
 		}
-	}
+		in.close();
 
-	private static int share(int[] choco, int minIndex, int maxIndex) {
-		int iterations = 0;
-		int give;
-		int tmpMin;
-		int tmpMax;
-		while(choco[minIndex] != choco[maxIndex] && iterations < 1024) {
-			tmpMin = choco[minIndex];
-			tmpMax = choco[maxIndex];
-			give = christy(tmpMin, tmpMax);
-			for (int i = 0; i < choco.length; i++) {
-				if (i != maxIndex) {
-					choco[i] += give;
-				}
-				if(choco[i] > tmpMax) { maxIndex = i; tmpMax = choco[maxIndex]; }
-			}
-			System.out.println("Min = " + choco[minIndex] + "\tMax = " + choco[maxIndex] + "\tGive = " + give);
-			show(choco);
-			iterations++;
-		}
-		return iterations;
-	}
-
-	private static int christy(int min, int max) {
-		if(max - min >= 5) { return 5; }
-		if(max - min >= 2) { return 2; }
-		return 1;
-	}
-
-	private static void show(int[] block) {
-		for (int chocolate : block) {
-			System.out.print(chocolate + "\t");
+		for (int item : list) {
+			System.out.print(item + "\t");
 		}
 	}
 }
