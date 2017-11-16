f2c8e501c6d757549bc24ced245477ba238c3c03
wthered <wthered@gmail.com>
2017-11-16 17:27:10 +0200

    Week of Code Nov 2017
    Signed-off-by: wthered <wthered@gmail.com>

--- src/Solution.java
+++ src/Solution.java
@@ -1,46 +1,31 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
 
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		int many = input.nextInt();
-		boolean[] results = new boolean[many];
-		for(int i=0; i<many; i++) {
-			int n = input.nextInt();
-			int[] numbers = new int[n];
+	private static void tripleRecursion(int n, int m, int k) {
+		int[][] a = new int[n][n];
+		a[0][0] = m;
+		for(int i=0; i<n; i++) {
 			for(int j=0; j<n; j++) {
-				numbers[j] = input.nextInt();
+				if(i < j) { a[i][j] = a[i][j-1] - 1; }
+				if(i == j && i>1 && j>1){ a[i][j] = a[i-1][j-1] + k; }
+				if(i > j && i>1) { a[i][j] = a[i-1][j] - 1; }
+				System.out.print(a[i][j] + "\t");
 			}
-			int gcd = getGCD(numbers);
-			results[i] = gcd >= 2 && exists(numbers, gcd);
-		}
-		input.close();
-
-		for(boolean result : results) {
-			System.out.println(result ? "NO" : "YES");
+			System.out.println();
 		}
 	}
 
-	private static boolean exists(int[] numbers, int gcd) {
-		boolean present = false;
-		for(int i=0; i < numbers.length; i++) {
-			if(numbers[i] == gcd) {
-				present = true;
-				i = numbers.length;
-			}
-		}
-		return present;
-	}
-
-	private static int getGCD(int[] numbers) {
-		int q = numbers[0];
-		int p = numbers[numbers.length - 1];
-		while (q != 0) {
-			int temp = q;
-			q = p % q;
-			p = temp;
-		}
-		return p;
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int m = in.nextInt();
+		int k = in.nextInt();
+		tripleRecursion(n, m, k);
+		in.close();
 	}
 }
